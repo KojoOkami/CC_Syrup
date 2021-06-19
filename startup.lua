76c3026d463d5b8c3a387f672a9b792bc7929a5c
Kojo Okami <kojookami@gmail.com>
2021-06-19 14:29:13 -0600

    init epsilon

--- startup.lua
+++
@@ -1,9 +0,0 @@
-rednet.open("left")
-while true do
-  local sender, message, protocol, = rednet.receive()
-  if message == "butler show" then
-    shell.run("butler show")
-  elseif message == "butler hide" then
-    shell.run("butler hide")
-  end
-end
